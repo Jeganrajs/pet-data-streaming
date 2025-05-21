5d5b066513e16c34662cc9b92aff25f00338e8f2
jeganrajs <jeganraj.sahayar@tigeranalytics.com>
2025-05-21 19:38:17 +0530

    Restructured  raw to prep job

--- src/data_processor/r2p_orders.py
+++ src/data_processor/r2p_orders.py
@@ -20,21 +20,61 @@ spark.sparkContext.setLogLevel("ERROR")
 
 data_dir = spark_config["data_dir"]
 checkpoint_loc = os.path.join(data_dir,"checkpoints","r2p_orders")
-
-users_df = spark.read.parquet(os.path.join(data_dir,"brz_online_users"))
-orders_df = spark.read.parquet(os.path.join(data_dir,"brz_transactions"))
-res_df = orders_df.where("loaded_time = '2025-05-19 19:03:26' ")
-
-#  Save online users data into Postgres table
-url = "jdbc:postgresql://localhost:5432/online_orders"
+# url = "jdbc:postgresql://localhost:5432/online_orders"
 pg_properties = {"user": pg_config['pg_user'],"password": pg_config['pg_pwd'],"driver": pg_config['pg_driver']}
-# print(f"postgres cong : {pg_properties}")
-print("Started saving resluts into postgres users table")
-# users_df.write.jdbc(url=pg_config['pg_url'], table="users", mode="overwrite", properties=pg_properties)
-print("******** Reults saved into users table ************")
 
-# Save Transactions data into posgress table
-print("Started saving resluts into postgres Transactions table")
-res_df.write.jdbc(url=pg_config['pg_url'], table="orders", mode="append", properties=pg_properties)
-print("******** Reults saved into Transactions table ************")
+def process_users_table():
+    query = "(SELECT count(*) as loaded_time FROM public.users ) AS subquery"
 
+    df = (spark.read 
+        .format("jdbc")
+        .option("driver",pg_config['pg_driver'])
+        .option("url", pg_config['pg_url'])
+        .option("dbtable", query)
+        .option("user", pg_config['pg_user'])
+        .option("password", pg_config['pg_pwd'])
+        .load()
+    )
+    if df.isEmpty():
+        users_df = spark.read.parquet(os.path.join(data_dir,"brz_online_users"))
+
+        #  Save online users data into Postgres table
+        print("Started saving resluts into postgres users table")
+        users_df.write.jdbc(url=pg_config['pg_url'], table="users", mode="overwrite", properties=pg_properties)
+        print("******** Reults saved into users table ************")
+    else:
+        print("Users table is already exist.. ")
+
+def process_orders_table():
+    """ Process orders table to postgress """
+
+    orders_df = spark.read.parquet(os.path.join(data_dir,"brz_transactions"))
+    # Get max timestamp from postgres table        
+    query = "(SELECT max(loaded_time) as loaded_time FROM public.orders ) AS subquery"
+
+    df = (spark.read 
+        .format("jdbc")
+        .option("driver",pg_config['pg_driver'])
+        .option("url", pg_config['pg_url'])
+        .option("dbtable", query)
+        .option("user", pg_config['pg_user'])
+        .option("password", pg_config['pg_pwd'])
+        .load()
+    )
+    checkpoint_time = df.collect()[0][0]
+
+    res_df = orders_df.where(f"to_timestamp(loaded_time) > to_timestamp('{checkpoint_time}') ")
+    cnt = res_df.count()
+    print(f"Number of records processed for orders table :: {cnt}")
+
+    if cnt>0:
+        # Save Transactions data into posgress table
+        print("Started saving resluts into postgres Transactions table")
+        res_df.write.jdbc(url=pg_config['pg_url'], table="orders", mode="append", properties=pg_properties)
+        print("******** Reults saved into Transactions table ************")
+    else:
+        print("No new record to process for orders table...!")
+
+if __name__ == "__main__":
+    process_users_table()
+    process_orders_table()
